6535b14c56b16f7b184bc4866e9d8c7e2d5cc33a
keanuyulexar <keanuyule764@gmail.com>
2025-05-24 11:38:53 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner =new Scanner(System.in);
+
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.println("Enter the temperature");
+        temp = scanner.nextDouble();
+
+        System.out.println("Convert to Celsius or Fahrenheit? (C or F): ");
+        unit = scanner.next().toUpperCase(Locale.ROOT);
+
+        // (condition) ? true : false
+
+       newTemp = (unit.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 5 / 9) + 32;
+
+        System.out.printf("%.1f%s", newTemp , unit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        scanner.close();
+    }
+}
